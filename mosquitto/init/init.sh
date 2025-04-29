2f9c3606b85de484736fec57a0cb1f91a1000bf7
evgeniy.glebov <evgeniy.glebov@quadcode.com>
2025-04-29 16:11:36 +0300

    mosq conf

--- mosquitto/init/init.sh
+++ mosquitto/init/init.sh
@@ -2,16 +2,14 @@
 
 set -e
 
-# Создание директорий, если их нет
-mkdir -p /mosquitto/config
-mkdir -p /mosquitto/data
-mkdir -p /mosquitto/log
+# Создание директорий
+mkdir -p /mosquitto/config /mosquitto/data /mosquitto/log
 
 # Назначение прав
 chown -R mosquitto:mosquitto /mosquitto
 
-# Генерация passwordfile (от имени mosquitto)
-su mosquitto -c "mosquitto_passwd -b -c /mosquitto/config/passwordfile \"$MQTT_USER\" \"$MQTT_PASSWORD\""
+# Генерация passwordfile от имени mosquitto
+gosu mosquitto mosquitto_passwd -b -c /mosquitto/config/passwordfile "$MQTT_USER" "$MQTT_PASSWORD"
 
-# Запуск mosquitto (от имени mosquitto)
-exec su mosquitto -c "mosquitto -c /mosquitto/config/mosquitto.conf"
+# Запуск Mosquitto от имени mosquitto
+exec gosu mosquitto mosquitto -c /mosquitto/config/mosquitto.conf
