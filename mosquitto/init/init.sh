57ae80bb02427a095069a21a3d4523bf21df417e
evgeniy.glebov <evgeniy.glebov@quadcode.com>
2025-04-29 12:50:43 +0300

    docker fix

--- mosquitto/init/init.sh
+++ mosquitto/init/init.sh
@@ -2,6 +2,10 @@
 
 PASSFILE="/mosquitto/config/passwordfile"
 
+chown -R mosquitto:mosquitto /mosquitto/config
+chown -R mosquitto:mosquitto /mosquitto/log
+chown -R mosquitto:mosquitto /mosquitto/data
+
 if [ ! -f "$PASSFILE" ]; then
   echo "Generating Mosquitto password file..."
   mosquitto_passwd -b -c "$PASSFILE" "${MQTT_USER}" "${MQTT_PASSWORD}"
